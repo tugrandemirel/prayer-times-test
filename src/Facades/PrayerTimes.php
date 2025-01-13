e59cf172f93e40630dac58e47cd56e7fd36b1a3c
Tuğran Demirel <49235236+tugrandemirel@users.noreply.github.com>
2025-01-14 01:48:14 +0300

    Update PrayerTimes.php

--- src/Facades/PrayerTimes.php
+++ src/Facades/PrayerTimes.php
@@ -6,11 +6,8 @@ use Illuminate\Support\Facades\Facade;
 
 class PrayerTimes extends Facade
 {
-    /**
-     * Get the registered name of the component.
-     */
     protected static function getFacadeAccessor(): string
     {
         return 'prayer-times';
     }
-} 
+}
