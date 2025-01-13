1e9e830a5d1d38eb08d26d919c12b0c2e64a5f1b
Tuğran Demirel <49235236+tugrandemirel@users.noreply.github.com>
2025-01-14 01:45:41 +0300

    Update PrayerTimesServiceProvider.php

--- src/PrayerTimesServiceProvider.php
+++ src/PrayerTimesServiceProvider.php
@@ -8,12 +8,8 @@ use Tugrandemirel\PrayerTimes\Services\PrayerTimesService;
 
 class PrayerTimesServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
-        // Config dosyasını merge et
         $this->mergeConfigFrom(
             __DIR__ . '/Config/prayer-times.php', 'prayer-times'
         );
@@ -22,20 +18,15 @@ class PrayerTimesServiceProvider extends ServiceProvider
             return new AladhanApiService();
         });
 
-        // PrayerTimesService'i singleton olarak kaydet
         $this->app->singleton('prayer-times', function ($app) {
             return new PrayerTimesService($app->make(AladhanApiService::class));
         });
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
-        // Config dosyasını publish et
         $this->publishes([
             __DIR__ . '/Config/prayer-times.php' => config_path('prayer-times.php'),
         ], 'prayer-times-config');
     }
-} 
+}
